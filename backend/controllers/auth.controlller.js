9c30a4ec9a6e8116353971cf9f431226e237ee29
Dinesh S <dineshindevelope@gmail.com>
2024-11-30 22:44:01 +0530

    first commit

---
+++ backend/controllers/auth.controlller.js
@@ -0,0 +1,15 @@
+export const signup =(req,res)=>{
+    console.log("SignUp User");
+    
+}
+
+
+export const login =(req,res)=>{
+    console.log("Login User");
+    
+}
+
+export const logout =(req,res)=>{
+    console.log("Logut User");
+    
+}
