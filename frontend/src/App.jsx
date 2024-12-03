fb2d02d21a527d0ce91b6a0bf125c8c59715724b
Dinesh S <dineshindevelope@gmail.com>
2024-12-03 22:58:48 +0530

    Frontend-model-design-completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
-import Login from "./assets/pages/Login/Login.jsx"
-
+import Home from "./assets/pages/Home/Home.jsx";
+import Login from "./assets/pages/Login/Login.jsx";
+import Signup from "./assets/pages/Signup/Signup.jsx";
 
 function App() {
- 
-
   return (
     <div className="p-4 h-screen flex justify-center items-center">
-    <Login />
-   
+      {/*  <Login /> */}
+      {/* <Signup /> */}
+      <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
